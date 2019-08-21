12a20064ab38ee76583a4bcc1d0196125e1b726f
Kakeru Fukuda <stand.by.we@gmail.com>
2019-08-21 12:00:56 +0900

    ついか

--- PromisePlayground/ViewController.swift
+++ PromisePlayground/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import PromiseKit
+import ObjectiveC
 
 class ViewController: UIViewController {
 
+    private func hoge() -> Int { return 1 }
+    private func hoge() -> String { return "" }
+
+    @IBOutlet private weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        label.text = "private label" + hoge()
+
+        firstly {
+            hoge()
+        }.then { result -> Promise<Int> in
+            if result == "aa" {
+                print("a")
+                return Promise(  Promise(value: 10)
+            }
+
+            return self.hoge2()
+        }.done { x in
+            print("done + " + x.description)
+        }
     }
 
+    func hoge() -> Promise<String> {
+        return Promise<String> { resolver in
+            print("hoge")
+            resolver.fulfill("aaa")
+        }
+    }
+
+    func hoge2() -> Promise<Void> {
+        return Promise<Void> { resolver in
+            print("hoge2")
+//            resolver.fulfill(())
+        }
+    }
 
 }
 
