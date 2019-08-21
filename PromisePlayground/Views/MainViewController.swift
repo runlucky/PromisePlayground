1f1b03e9d82a99b852b898536fee60a989a9c365
Kakeru Fukuda <stand.by.we@gmail.com>
2019-08-21 15:21:50 +0900

    PromiseKitがクソなきがする。

--- PromisePlayground/Views/MainViewController.swift
+++ PromisePlayground/Views/MainViewController.swift
@@ -12,6 +12,67 @@ import PromiseKit
 class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        formatter.dateFormat = "HH:mm:ss"
+
+        firstly {
+            a()
+        }.then {
+            log("end")
+        }.catch { error in
+            log("error")
+        }
+
+    }
+
+    func a() -> Promise<Void> {
+        return Promise<Void> { fulfill, reject in
+            firstly {
+                Waiter([1]).wait()
+            }.then { result -> Promise<([TimeInterval], [TimeInterval])> in
+                when(fulfilled:
+                        Waiter(result).wait(),
+                    Waiter([-1]).wait()
+                )
+            }.then { _ in
+                fulfill(())
+            }.catch { error in
+                reject(error)
+            }
+        }
     }
 }
 
+
+enum PromiseError: Error {
+    case invalid
+}
+
+class Waiter {
+    private let second: [TimeInterval]
+    init(_ second: [TimeInterval]) {
+        self.second = second
+    }
+
+    func wait() -> Promise<[TimeInterval]> {
+        if second.first! < 0 {
+            return Promise(error: PromiseError.invalid)
+        }
+
+        return Promise<[TimeInterval]> { fulfill, reject in
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.second.first!) {
+                log("fulfill: " + self.second.description)
+                fulfill(self.second)
+            }
+        }
+    }
+}
+
+func log(_ text: String) {
+    print(now + " " + text)
+}
+
+var now: String {
+    return formatter.string(from: Date())
+}
+
+let formatter = DateFormatter()
